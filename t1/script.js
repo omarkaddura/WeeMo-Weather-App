9e73b69cad9070a340ece593db0e76766b91f716
SinisterNight <114369787+SinisterNight@users.noreply.github.com>
2022-12-11 18:27:31 -0600

    Update script.js

--- t1/script.js
+++ t1/script.js
@@ -5,15 +5,15 @@ function getTime(){
     const date = new Date();
 
     const seconds = date.getSeconds();
-    const sDegrees = ((seconds / 60) * 360); //+ 90;
+    const sDegrees = ((seconds / 60) * 360);
     secondHand.style.transform = `rotate(${sDegrees}deg)`;
 
     const minutes = date.getMinutes();
-    const mDegrees = ((minutes / 60) * 360); //+ ((seconds / 60) * 6) + 90;
+    const mDegrees = ((minutes / 60) * 360); 
     minuteHand.style.transform = `rotate(${mDegrees}deg)`;
 
     const hours = date.getHours();
-    const hDegrees = ((hours / 12) * 360); //+ ((minutes / 60) * 30) + 90;
+    const hDegrees = ((hours / 12) * 360);
     hourHand.style.transform = `rotate(${hDegrees}deg)`;
     
 
